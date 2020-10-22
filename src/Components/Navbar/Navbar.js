a11af756349414703521ccea45356c54703f07c0
Levani Papashvili <papashvililevani@gmail.com>
2020-10-22 16:51:09 -0400

    add soem code in Navbar.js & Icons

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+// import "./Navbar.css";
+import { FaHome, FaCamera, FaThLarge, FaCog } from "react-icons/fa";
 
-function Navbar() {
-  return (
-    <div>
-      <h1>NavBar helo</h1>
-    </div>
-  );
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar__Container">
+        <div className="container">
+          <div className="icon home">
+            <i className="fab fa-icon">
+              <FaHome />
+            </i>
+          </div>
+          <div className="icon gallery">
+            <i className="fab fa-gallery">
+              <FaThLarge />
+            </i>
+          </div>
+          <div className="icon Camera">
+            <i className="fab fa-Camera">
+              <FaCamera />
+            </i>
+          </div>
+          <div className="icon FaCog">
+            <i className="fab fa-FaCog">
+              <FaCog />
+            </i>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
