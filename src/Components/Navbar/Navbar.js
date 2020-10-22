3eb067838bb40f15a4dd4c221f0aa3c5030ce8bf
Levani Papashvili <papashvililevani@gmail.com>
2020-10-22 17:02:45 -0400

    add Navbar.css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import "./Navbar.css";
+import "./Navbar.css";
 import { FaHome, FaCamera, FaThLarge, FaCog } from "react-icons/fa";
 
 class Navbar extends Component {
