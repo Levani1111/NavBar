d61f295650c025f2fb8aeee6c64dc1bc468ef3c4
Levani Papashvili <papashvililevani@gmail.com>
2020-10-22 16:44:12 -0400

    created Components/Navbar/Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-// import NavBar from "./Components/Navbar/Navbar.js";
+import NavBar from "./Components/Navbar/Navbar.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>{/* <NavBar /> */}</div>
+        <div>
+          <NavBar />
+        </div>
       </header>
     </div>
   );
