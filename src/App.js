cd7118dfd9d4d4e3f87e1e6b170b3ff271edce82
Levani Papashvili <papashvililevani@gmail.com>
2020-10-22 16:40:53 -0400

    create header & changed  backgroung color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-
 import "./App.css";
+// import NavBar from "./Components/Navbar/Navbar.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>{/* <NavBar /> */}</div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
